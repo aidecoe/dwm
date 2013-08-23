d7381c6752a8972b007f03bd1754a0f827110eae
Jente Hidskes <jthidskes@outlook.com>
2013-08-23 20:22:42 +0200

    deck
    deck is a new layout for DWM (inspired by the TTWM window manager). It
    is like tile(), but with monocle in the stack. You have a master client
    (complete with nmaster et all), but the stack clients now overlap one
    another as in monocle(). (The stacked clients are like a deck of cards,
    hence the name).
    
    Tile :
    +-----------------+--------+
    |                 |        |
    |                 |  S1    |
    |                 |        |
    |        M        +--------+
    |                 |        |
    |                 |   S2   |
    |                 |        |
    +-----------------+--------+
    
    Deck :
    +-----------------+--------+
    |                 |        |
    |                 |        |
    |                 |        |
    |        M        |   S1   |
    |                 |        |
    |                 |        |
    |                 |        |
    +-----------------+--------+
    
    URL: http://dwm.suckless.org/patches/deck

--- config.def.h
+++ config.def.h
@@ -31,6 +31,7 @@ static const Bool resizehints = True; /* True means respect size hints in tiled 
 
 #include "bstack.c"
 #include "bstackhoriz.c"
+#include "deck.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
@@ -38,6 +39,7 @@ static const Layout layouts[] = {
 	{ "[M]",      monocle },
 	{ "TTT",      bstack },
 	{ "===",      bstackhoriz },
+	{ "[]|",      deck },
 };
 
 /* key definitions */
